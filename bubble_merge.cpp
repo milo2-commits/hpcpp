12fe54f924fc5d2d94f752f26b199f2c79802f9d
milomilo <tammanasharma1208@gmail.com>
2026-05-06 16:41:21 +0530

    Update bubble_merge.cpp

--- bubble_merge.cpp
+++ bubble_merge.cpp
@@ -154,3 +154,17 @@ int main() {
 }
 
 // openmp command= -fopenmp
+
+// g++ 4_parallel_quicksort.cpp -fopenmp -o quick
+// quick
+
+//g++ 2_parallel_bubble_merge.cpp -fopenmp -o par_bub
+//par_bub
+
+// # Compile and run parallel quicksort
+// g++ 4_parallel_quicksort.cpp -fopenmp -o quick
+// ./quick
+
+// # Compile and run parallel bubble merge
+// g++ 2_parallel_bubble_merge.cpp -fopenmp -o par_bub
+// ./par_bub
