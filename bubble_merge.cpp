f2c4dd5435a7039a935c56b83d2e2443d16e6ac7
milomilo <tammanasharma1208@gmail.com>
2026-05-08 10:05:24 +0530

    Update bubble_merge.cpp

--- bubble_merge.cpp
+++ bubble_merge.cpp
@@ -153,6 +153,24 @@ int main() {
     return 0;
 }
 
+
+# Open Terminal
+Ctrl + Alt + T
+
+# Open Gedit
+gedit bfs_dfs.cpp
+
+# Install g++ Compiler
+sudo apt install g++
+
+# Install OpenMP Support
+sudo apt install libomp-dev
+
+# Compile the Code
+g++ bfs_dfs.cpp -fopenmp -o output
+
+# Run the Program
+./output
 // openmp command= -fopenmp
 
 // g++ 4_parallel_quicksort.cpp -fopenmp -o quick
